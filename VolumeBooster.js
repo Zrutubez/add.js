0136ae9eab0379e88650ca9431736fd719955af1
Zrutubez <163627430+Zrutubez@users.noreply.github.com>
2024-11-16 21:35:51 +0700

    Update VolumeBooster.js

--- VolumeBooster.js
+++ VolumeBooster.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name         Volume Booster
-// @namespace    https://greasyfork.org/zh-CN/users/196399-xyabc120
-// @version      2024-09-27
 // @description  Control volume of playing media on any webpage
-// @author       mr.zhao
 // @match        *://*.Youtube.com/*
 // @icon         https://M.youtube.com/favicon.ico?v=1
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @license      MIT
-// @downloadURL https://update.greasyfork.org/scripts/510425/Volume%20Booster.user.js
-// @updateURL https://update.greasyfork.org/scripts/510425/Volume%20Booster.meta.js
+// @downloadURL https://raw.githubusercontent.com/Zrutubez/add.js/refs/heads/main/VolumeBooster.js
+// @updateURL https://raw.githubusercontent.com/Zrutubez/add.js/refs/heads/main/VolumeBooster.js
 // ==/UserScript==
 
 (function() {
@@ -20,13 +17,13 @@
     let currentGainIndex = GM_getValue(storageKeyName, 0);
     const gainValues = [1.0, 2.0, 3.0, 4.0, 5.0];
 
-    // 创建 AudioContext 和 GainNode
+    
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     const gainNode = audioContext.createGain();
     gainNode.gain.value = gainValues[currentGainIndex];
 
     let connectedVideo = null;
-    // 尝试连接现有视频元素
+    
     const connectMedia = () => {
         const videoElement = document.querySelector('video');
         if (videoElement && videoElement !== connectedVideo) {
@@ -40,11 +37,11 @@
         }
     };
 
-    // 监听 DOM 变化
+    
     const observer = new MutationObserver(connectMedia);
     observer.observe(document.body, { childList: true, subtree: true });
 
-    // 可选：创建一个按钮来调整音量
+
     const button = document.createElement('button');
     button.textContent = `Volume Boost ${gainValues[currentGainIndex]}x`;
     button.style.position = 'fixed';
@@ -52,7 +49,7 @@
     button.style.right = '10px';
     button.style.zIndex = '9999';
     button.style.padding = "1px 7px";
-    button.title = "左键点击增加音量倍数，右键重置";
+    button.title = "Left click to increase the volume multiplier, right click to reset";
     document.body.appendChild(button);
 
     const updateGain = (gainIndex) => {
